0adadc2074d1cb5077c58c637c53280e6849650e
HLuuVN <hluuvn@gmail.com>
2025-12-24 12:46:21 +0700

    Update HRMIOp4kG29LCkGwj0hX.lua

--- HRMIOp4kG29LCkGwj0hX.lua
+++ HRMIOp4kG29LCkGwj0hX.lua
@@ -1,4 +1,4 @@
-loadstring(game:HttpGet(("https://raw.githubusercontent.com/hluuvn/Roblox/refs/heads/main/main.lua")))()
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/hluuvngaming/Roblox/refs/heads/main/main.lua")))()
 
        local Window = MakeWindow({
          Hub = {
